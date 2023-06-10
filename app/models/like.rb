862909898c7cc6c7545a004d587bf5f6046ceadd
Newtayo <akandeabdul@gmail.com>
2023-06-10 18:36:01 +0100

    Set up validation and test validation

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,12 +1,13 @@
 class Like < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  belongs_to :post, class_name: 'Posts'
+    belongs_to :author, class_name: 'User'
+    belongs_to :post, class_name: 'Post'
 
   after_save :update_like_count
 
   private
 
   def update_like_count
-    post.increment!(:likes_counter)
+    Post.increment_counter(:likes_counter, self.post_id)
   end
+  
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,25 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  has_many :likes
-  has_many :comments
-
-  after_save :update_posts_count
-
-  def update_posts_count
-    author.increment!(:post_counter)
+    belongs_to :author, class_name: 'User'
+    has_many :likes, class_name: 'Like'
+    has_many :comments
+  
+    after_save :update_posts_count
+  
+    def update_posts_count
+      author.increment!(:post_counter)
+    end
+  
+    def recent_comments
+      comments.order(created_at: :desc).limit(5)
+    end
+    validates :title, presence: true, length: { maximum: 250 }
+    validates :comments_counter,
+              numericality: { only_integer: true, greater_than_or_equal_to: 0,
+                              message: 'must be an integer greater than or equal to zero.' }
+    validates :likes_counter,
+              numericality: { only_integer: true, greater_than_or_equal_to: 0,
+                              message: 'must be an integer greater than or equal to zero.' }
   end
-
-  def recent_comments
-    comments.order(created_at: :desc).limit(5)
-  end
-end
+  
+ 
+  
