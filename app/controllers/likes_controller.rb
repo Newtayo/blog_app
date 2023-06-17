2762b28fadb1f011cfda2d2e3b12ac7a70135200
Newtayo <akandeabdul@gmail.com>
2023-06-17 16:20:02 +0100

    Set up forms for post, comments and likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,13 @@
 class LikesController < ApplicationController
+  def create
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:post_id])
+    @like = @post.likes.new(author: current_user)
+
+    if @like.save
+      redirect_to user_post_path(@user, @post)
+    else
+      redirect_to new_user_post_like_path(@user, @post), notice: 'Failed to like the post.'
+    end
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,4 +13,24 @@ class PostsController < ApplicationController
             end
     @comments = @post.comments
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @user = current_user
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to user_posts_path(@user), notice: 'Post created successfully.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
