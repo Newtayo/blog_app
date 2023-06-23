942f929455c3232107a4ffc90ab974a4bd53f9c9
Newtayo <akandeabdul@gmail.com>
2023-06-23 12:42:50 +0100

    Setup ability

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,18 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
   has_many :likes, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :posts, foreign_key: 'author_id'
+
+  ROLES = %w[admin member].freeze
+
+  ROLES.each do |role_name|
+    define_method "#{role_name}?" do
+      role == role_name
+    end
+  end
 
   def three_recent_posts
     posts.order(created_at: :desc).limit(3)
